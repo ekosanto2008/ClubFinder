933467e01cbc3f4bcab3385ec3b4e330e1d564d4
ekosanto2008 <ekosanto2008@bsi.ac.id>
2020-03-22 11:26:18 +0700

    CLub Finder

---
+++ app.js
@@ -0,0 +1,5 @@
+import './src/script/component/seacrh-bar.js';
+import main from "./src/script/view/main.js";
+import "./src/script/component/app-bar.js";
+
+document.addEventListener("DOMContentLoaded", main);
